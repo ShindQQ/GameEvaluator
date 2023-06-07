1d760b9690d255c7de5aef4888dea4f196b038c7
ShindQQ <denis110402@gmail.com>
2023-06-07 12:59:33 +0300

    Added health check

---
+++ Presentration.API/Health/DatabaseHealthCheck.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Presentration.API.Health;
+
+public sealed class DatabaseHealthCheck : IHealthCheck
+{
+    private string ConnectionString { get; init; }
+
+    public DatabaseHealthCheck(IConfiguration configuration)
+    {
+        ConnectionString = configuration.GetConnectionString("DefaultConnection")!;
+    }
+
+    public async Task<HealthCheckResult> CheckHealthAsync(
+        HealthCheckContext context, 
+        CancellationToken cancellationToken = new())
+    {
+        using var connection = new SqlConnection(ConnectionString);
+
+        try
+        {
+            await connection.OpenAsync(cancellationToken);
+            var command = connection.CreateCommand();
+            command.CommandText = "SELECT 1";
+            await command.ExecuteNonQueryAsync(cancellationToken);
+        }
+        catch (Exception exception)
+        {
+            return HealthCheckResult.Unhealthy(exception: exception);
+        }
+
+        return HealthCheckResult.Healthy();
+    }
+}
--- Presentration.API/Program.cs
+++ Presentration.API/Program.cs
@@ -1,9 +1,13 @@
 using Application;
 using Hangfire;
 using HangfireBasicAuthenticationFilter;
+using HealthChecks.UI.Client;
 using Infrastructre;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Presentration.API;
 using Presentration.API.BackgroundJobs;
+using Presentration.API.Health;
 using Presentration.API.Hubs;
 using Presentration.API.Middlewares;
 using Serilog;
@@ -13,12 +17,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Host.UseSerilog((context, configuration) =>
     configuration.ReadFrom.Configuration(context.Configuration));
 
+builder.Services.AddHealthChecks()
+    .AddCheck<DatabaseHealthCheck>("Database");
+
 builder.Services
     .AddApplication()
     .AddInfrastructure(builder.Configuration)
     .AddAPI(builder.Configuration);
 
 var app = builder.Build();
+
+app.MapHealthChecks("/_health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+}).RequireAuthorization(new AuthorizeAttribute() { Roles = "SuperAdmin", });
 
 app.UseMiddleware<ExceptionMiddleware>();
 
